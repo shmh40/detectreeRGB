cb0d0aba8e5a20c807886fbfdafcb1f24afc934e
PatBall1 <ball.jgc@gmail.com>
2021-08-13 16:40:58 +0100

    crown prep

--- tiling/tiling.R
+++ tiling/tiling.R
@@ -4,8 +4,18 @@ library(sf)
 library(terra)
 library(rgeos)
 
+library(parallel)
+
+ncores <- detectCores()
+cl <- makeCluster(ncores)
 
 # Load in crowns - needed for both the single loop example, and the loop further down
-Sep_crowns=st_read("~/lustre_scratch/sepilok_data/thomas_sepilok_crowns/thomas_no_intersection.shp")
+crowns <- st_read("~/lustre_scratch/sepilok_data/thomas_sepilok_crowns/thomas_no_intersection.shp")
 
-splitRaster(r, nx, ny, buffer, path, cl)
+
+raster <- 
+origin <- origin(raster)
+tiledRaster <- splitRaster(r, nx, ny, buffer, path, cl)
+
+
+stopCluster(cl)
